b161fd9ff3b2179d2d17bcf7a1d610686c3a784f
Tharana Navithaka Bopearachchi <tharanabope30@gmail.com>
2025-08-19 11:29:48 +0530

    Norminal and Ordinal feature encoding

--- Building Production Ready ML Systems/Week 05_06/src/feature_encoding.py
+++ Building Production Ready ML Systems/Week 05_06/src/feature_encoding.py
@@ -8,40 +8,47 @@ from abc import ABC, abstractmethod
 logging.basicConfig(level=logging.INFO, format=
     '%(asctime)s - %(levelname)s - %(message)s')
 
-
+# creating abstract class for feature encoding strategies
 class FeatureEncodingStrategy(ABC):
     @abstractmethod
     def encode(self, df: pd.DataFrame) ->pd.DataFrame:
         pass
 
-
+# creating enum for variable types
 class VariableType(str, Enum):
     NOMINAL = 'nominal'
     ORDINAL = 'ordinal'
 
+# creating nominal encoding strategy
 class NominalEncodingStrategy(FeatureEncodingStrategy):
+    # Constructor for NominalEncodingStrategy
     def __init__(self, nominal_columns):
         self.nominal_columns = nominal_columns
         self.encoder_dicts = {}
         os.makedirs('artifacts/encode', exist_ok=True)
 
+    # Encode the DataFrame using nominal encoding
     def encode(self,df):
         for column in self.nominal_columns:
             unique_values = df[column].unique()
             encoder_dict = {value: i for i, value in enumerate(unique_values)}
             self.encoder_dicts[column] = encoder_dict
 
-            encoder_path = os.path.join('artifacts/encode', f"{column}_encoder.json")
+            encoder_path = os.path.join(
+                'artifacts/encode', f"{column}_encoder. json"
+                )
             with open(encoder_path, "w") as f:
                 json.dump(encoder_dict, f)
 
             df[column] = df[column].map(encoder_dict)
         return df
-    
+
+    # Get the encoder dictionaries
     def get_encoder_dicts(self):
         return self.encoder_dicts
-    
-class OrdinalEncodingStratergy(FeatureEncodingStrategy):
+
+# creating ordinal encoding strategy
+class OrdinalEncodingStrategy(FeatureEncodingStrategy):
     def __init__(self, ordinal_mappings):
         self.ordinal_mappings = ordinal_mappings
 
