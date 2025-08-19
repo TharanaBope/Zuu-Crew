e60c9eb4b8923f6d9f0060c9d290cc639de8590b
Tharana Navithaka Bopearachchi <tharanabope30@gmail.com>
2025-08-19 11:24:53 +0530

    feature binning

--- Building Production Ready ML Systems/Week 05_06/src/feature_binning.py
+++ Building Production Ready ML Systems/Week 05_06/src/feature_binning.py
@@ -4,21 +4,25 @@ from abc import ABC, abstractmethod
 logging.basicConfig(level=logging.INFO, format=
     '%(asctime)s - %(levelname)s - %(message)s')
 
-
+# creating abstract class for feature binning strategies
 class FeatureBinningStrategy(ABC):
     @abstractmethod
     def bin_feature(self, df: pd.DataFrame, column: str) ->pd.DataFrame:
         pass
 
-class CustomBinningStratergy(FeatureBinningStrategy):
+# Creating a custom binning strategy
+class CustomBinningStrategy(FeatureBinningStrategy):
+    # Constructor for CustomBinningStrategy
     def __init__(self, bin_definitions):
         self.bin_definitions = bin_definitions 
 
+    # Bin the feature based on custom definitions
     def bin_feature(self, df, column):
         def assign_bin(value):
             if value == 850:
                 return "Excellent"
-            
+
+            # Assign bins based on custom definitions
             for bin_label, bin_range in self.bin_definitions.items():
                 if len(bin_range) == 2:
                     if bin_range[0] <= value <= bin_range[1]:
