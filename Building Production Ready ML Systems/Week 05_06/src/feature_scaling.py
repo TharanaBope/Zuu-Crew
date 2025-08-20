4a8e9c914b21e0f32cc87b6f53061d7be9afb372
Tharana Navithaka Bopearachchi <tharanabope30@gmail.com>
2025-08-20 22:07:55 +0530

    Featur Scalling

--- Building Production Ready ML Systems/Week 05_06/src/feature_scaling.py
+++ Building Production Ready ML Systems/Week 05_06/src/feature_scaling.py
@@ -19,16 +19,17 @@ class ScalingType(str, Enum):
     MINMAX = 'minmax'
     STANDARD = 'standard'
 
-class MinMaxScalingStratergy(FeatureScalingStrategy):
+class MinMaxScalingStrategy(FeatureScalingStrategy):
+    # Min-Max scaling strategy implementation
     def __init__(self):
         self.scaler = MinMaxScaler()
         self.fitted = False
-
+    # Abstract method implementation
     def scale(self, df, columns_to_scale):
         df[columns_to_scale] = self.scaler.fit_transform(df[columns_to_scale])
         self.fitted = True
         logging.info(f'Applied Min-Max scaling to columns: {columns_to_scale}')
-        return df 
-    
+        return df
+    # Getter for the scaler
     def get_scaler(self):
         return self.scaler
